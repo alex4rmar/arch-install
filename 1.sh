78947a940ce5d644dbcd352bf152a07ae4be956b
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-08-22 21:35:02 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 pacstrap /mnt base base-devel nano linux linux-firmware linux-headers dhcpcd networkmanager net-tools ifplugd openssh netctl dialog wireless_tools xf86-input-libinput bluez bluez-utils pulseaudio-bluetooth
 
 genfstab -U /mnt >> /mnt/etc/fstab
