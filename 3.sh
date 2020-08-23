09905ddd9c31aaf96da6288f33c444c16949f608
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-08-23 17:48:43 -0500

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-
-
-
+echo es_PE.UTF-8 UTF-8 > /etc/locale.gen
+locale-gen
 echo LANG=es_PE.UTF-8 > /etc/locale.conf
 export LANG=es_PE.UTF-8
+pacman -Sy curl --noconfirm
 ln -sf /usr/share/zoneinfo/America/Lima /etc/localtime
 hwclock -w
+echo KEYMAP=es > /etc/vconsole.conf
 echo ax > /etc/hostname
 echo -e "127.0.0.1	localhost\n::1		localhost\n127.0.1.1	ax.localdomain	ax" /etc/hosts
-
-
-
-pacman -Sy reflector python openssh networkmanager ifplugd xf86-input-synaptics --noconfirm
-reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
-
-systemctl enable NetworkManager
-systemctl enable sshd
-systemctl enable dhcpcd
-systemctl enable NetworkManager
-systemctl start NetworkManager.service
-systemctl enable sshd
-systemctl enable bluetooth
