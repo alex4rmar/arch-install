7a21a00d954cd96d4163569664edc6708cbc711a
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-08-25 20:24:39 -0500

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 pacman -Syu
-pacman -Sy git wget
-pacman -Sy neofetch lsb-release
-pacman -Sy intel-ucode
-pacman -S pulseaudio pavucontrol
-pacman -S xf86-video-intel lib32-mesa vulkan-intel
-pacman -S xdg-user-dirs
+pacman -Sy git wget --noconfirm
+pacman -Sy neofetch lsb-release --noconfirm
+pacman -Sy intel-ucode --noconfirm
+pacman -S pulseaudio pavucontrol --noconfirm
+pacman -S xf86-video-intel lib32-mesa vulkan-intel --noconfirm
+pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
 
 
 
-pacman -S xorg xorg-server xorg-apps xterm xorg-xinit mesa mesa-demos 
+pacman -S xorg xorg-server xorg-apps xterm xorg-xinit mesa mesa-demos --noconfirm
 
 localectl set-x11-keymap latam
 
-sudo pacman -Syy qtile alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger 
-sudo systemctl enable lightdm.service
+pacman -Syy qtile alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger --noconfirm
+systemctl enable lightdm.service
