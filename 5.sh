986dcf2475f5131bd01ec438468b5b5a16acc001
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-09-10 19:14:16 -0500

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -9,3 +9,4 @@ reflector --verbose --latest 15 --sort rate --save /etc/pacman.d/mirrorlist
 
 pacman -S grub efibootmgr os-prober --noconfirm
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=grub
+
