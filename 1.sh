40099c622edc9102c25f4b3a6db9d67359835e73
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-09-14 16:59:19 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -22,7 +22,7 @@ pacman -Sy reflector python --noconfirm
 reflector --verbose --latest 15 --sort rate --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel nano
 pacstrap /mnt linux-firmware linux linux-headers mkinitcpio
-pacstrap /mnt dhcpcd networkmanager iwd net-tools ifplugd 
+pacstrap /mnt dhcpcd networkmanager iwd net-tools ifplugd openssh
 pacstrap /mnt iw wireless_tools wpa_supplicant dialog wireless-regdb
 pacstrap /mnt xf86-input-libinput
 pacstrap /mnt bluez bluez-utils pulseaudio-bluetooth
