708d94d5dfb05d799f22b8b9d876f2a34ea3cb0c
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-09-14 17:43:57 -0500

    Update 6.sh

--- 6.sh
+++ 6.sh
@@ -9,23 +9,7 @@ pacman -S xf86-video-intel lib32-mesa vulkan-intel --noconfirm
 pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
 
-
-
 pacman -S xorg xorg-server xorg-apps xterm xorg-xinit mesa mesa-demos --noconfirm
 
 localectl set-x11-keymap latam
 
-#pacman -Syy qtile alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger --noconfirm
-#pacman -Syy qtile alacritty nitrogen picom lightdm lightdm-gtk-greeter  nemo --noconfirm
-#systemctl enable lightdm.service
-
-####################    bspwm
-
-#sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger 
-sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox nemo dmenu --noconfirm
-sudo systemctl enable lightdm.service
-mkdir .config/bspwm
-mkdir .config/sxhkd
-mkdir .config/polybar
-cp /usr/share/doc/bspwm/examples/bspwmrc .config/bspwm/
-cp /usr/share/doc/bspwm/examples/sxhkdrc .config/sxhkd/
