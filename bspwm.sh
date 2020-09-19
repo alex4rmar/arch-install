780275fee26e2f97774051908d3595ed266e796f
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-09-18 23:41:51 -0500

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -2,9 +2,20 @@
 
 pacman -Syy alacritty bspwm dialog ntfs-3g pulseaudio pulseaudio-alsa rofi sxhkd wireless_tools wpa_supplicant nitrogen picom lightdm lightdm-gtk-greeter firefox nemo dmenu --noconfirm
 sudo systemctl enable lightdm.service
+
+
+
 mkdir ~/.config/bspwm
 mkdir ~/.config/sxhkd
 mkdir ~/.config/polybar
+
+
+
+
 cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
+
+
+
+
 nano ~/.config/bspwm/bspwmrc
