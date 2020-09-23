e0bdd784ef32f3cf50a72adff7d569a58d4ab3d5
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-09-22 19:59:56 -0500

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-sudo pacman -Syy alacritty bspwm dialog ntfs-3g pulseaudio pulseaudio-alsa rofi sxhkd wireless_tools wpa_supplicant nitrogen picom lightdm lightdm-gtk-greeter firefox nemo dmenu --noconfirm
+
+
+#sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger ntfs-3g --noconfirm
+
+sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox nemo dmenu dialog ntfs-3g virtualbox-guest-utlis --noconfirm
+
 sudo systemctl enable lightdm.service
-
-
 
 sudo mkdir ~/.config/bspwm
 sudo mkdir ~/.config/sxhkd
 sudo mkdir ~/.config/polybar
 
-
-
-
 sudo cp /usr/share/doc/bspwm/examples/bspwmrc ~/.config/bspwm/
 sudo cp /usr/share/doc/bspwm/examples/sxhkdrc ~/.config/sxhkd/
-
+sudo cp /usr/share/doc/polybar/config ~/.config/polybar/
 
 sudo echo "exec bspwm" >  ~/.config/.xinitrc
 
