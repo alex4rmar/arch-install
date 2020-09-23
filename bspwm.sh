4d7d23b8aefafe0b4d81600b69de9dd94e8b478b
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-09-22 20:15:42 -0500

    Update bspwm.sh

--- bspwm.sh
+++ bspwm.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-
-
-#sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger ntfs-3g --noconfirm
+#	sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox ranger ntfs-3g --noconfirm
 
 sudo pacman -Syy bspwm sxhkd alacritty nitrogen picom lightdm lightdm-gtk-greeter firefox nemo dmenu dialog ntfs-3g virtualbox-guest-utlis --noconfirm
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+sudo rm -R yay
+
+git clone https://aur.archlinux.org/polybar-git.git
+cd polibar
+makepkg -si
+cd ..
+sudo rm -R polibar
 
 sudo systemctl enable lightdm.service
 
