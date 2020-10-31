d6c2504d50545a293a159eb746f9330c5424365d
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-10-30 21:44:22 -0500

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -4,12 +4,11 @@ sudo pacman -Syu
 sudo pacman -Sy git wget --noconfirm
 sudo pacman -Sy neofetch lsb-release --noconfirm
 sudo pacman -Sy intel-ucode --noconfirm
-sudo pacman -S pulseaudio pavucontrol --noconfirm
 sudo pacman -S xf86-video-intel lib32-mesa vulkan-intel --noconfirm
 sudo pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
-
+#
 sudo pacman -S xorg xorg-server xorg-apps xterm xorg-xinit mesa mesa-demos --noconfirm
-
+#
 localectl set-x11-keymap latam
 
