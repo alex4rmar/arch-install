9646fd8b272b5491874a90a6abbbe359367e4805
alex4rmar <43921988+alex4rmar@users.noreply.github.com>
2020-11-14 20:36:20 -0500

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -5,11 +5,8 @@ systemctl start dhcpcd
 #wipefs -a /dev/sda
 #sgdisk -Z /dev/sda
 gdisk /dev/sda
-
 gdisk /dev/sda
-
 gdisk /dev/sda
-
 mkfs.fat -F 32 /dev/sda1
 mkswap /dev/sda2
 swapon /dev/sda2
@@ -17,7 +14,6 @@ mkfs.ext4 /dev/sda3
 mount /dev/sda3 /mnt
 mkdir -p /mnt/boot/
 mount /dev/sda1 /mnt/boot
-
 pacman -Sy reflector python --noconfirm
 reflector --verbose --latest 15 --sort rate --save /etc/pacman.d/mirrorlist
 pacstrap /mnt base base-devel nano
